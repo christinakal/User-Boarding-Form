3fa48d3280702b8b4268519b0959080d110c5794
Christina <develop@pirron.one>
2019-12-20 00:09:50 +0100

    MAIN PR

--- src/UserForm.js
+++ src/UserForm.js
@@ -9,22 +9,27 @@ function UserForm() {
     return (
         <div>
             <Form>
+                {/* NAME FIELD */}
                 <div>
                     <label htmlFor="name">Name</label>
                     <Field id="name" type="text" name="name"/>
                 </div>
+                {/* EMAIL FIELD */}
                 <div>
                     <label htmlFor="email">Email</label>
                     <Field id="email" type="email" name="email"/>
                 </div>
+                {/* PASSWORD FIELD */}
                 <div>
                     <label htmlFor="password">Password</label>
                     <Field id="password" type="password" name="password"/>
                 </div>
+                {/* CHECKBOX */}
                 <div>
                 <label htmlFor="terms">I have read and accept the Terms of Service</label>
                     <Field id="terms" type="checkbox" name="terms"/>
                 </div>
+                <button>Submit!</button>
             </Form>
         </div>
     );
